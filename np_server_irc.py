c6757def17f02f8e97b3c1f49050e1dba6b6c193
R Somers <spamhole@telenet.be>
2013-04-08 20:50:04 +0200

    removed obsolete while

--- np_server_irc.py
+++ np_server_irc.py
@@ -23,29 +23,29 @@ def callback(host,port):
 		return 0
 #end def
 
+cSock, cAddr = s.accept()
+cFile = cSock.makefile('rw',0)
+cFile.write("irc.khleuven.be NOTICE AUTH :*** Looking up your hostname...\r\n")
 while 1:
-	cSock, cAddr = s.accept()
-	cFile = cSock.makefile('rw',0)
-	cFile.write("irc.khleuven.be NOTICE AUTH :*** Looking up your hostname...\r\n")
-	while 1:
-		line = cFile.readline().strip()
-		parts = line.split(" ")
-		print line
-		if parts[0]=="NICK":
-			cFile.write(":irc.khleuven.be 376 natpin252 :End of /MOTD command.\r\n")
-		elif parts[0] == "PRIVMSG":
-			if parts[3] == "CHAT":
-				numip = long(parts[5])
-				numip = socket.inet_ntoa(struct.pack('!I', numip))
-			
-				numport = parts[6].replace("\x01","")
-				callb_line = "Nat PIN " + cAddr[0] + "=> " + str(numip) + " on port " + numport
-				if callback(cAddr[0],int(numport)) == 1:
-					print callb_line + " :Success"
-				else:
-					print callb_line + " :Failed"
-				if EXIT_AFTER_CALLBACK==1:
-					s.close()
-					break
-			print parts
+	line = cFile.readline().strip()
+	parts = line.split(" ")
+	print line
+	if parts[0]=="NICK":
+		cFile.write(":irc.khleuven.be 376 natpin252 :End of /MOTD command.\r\n")
+	elif parts[0] == "PRIVMSG":
+		if parts[3] == "CHAT":
+			numip = long(parts[5])
+			numip = socket.inet_ntoa(struct.pack('!I', numip))		
+			numport = parts[6].replace("\x01","")
+			callb_line = "Nat PIN " + cAddr[0] + "=> " + str(numip) + " on port " + numport
+			if callback(cAddr[0],int(numport)) == 1:
+				print callb_line + " :Success"
+			else:
+				print callb_line + " :Failed"
+			if EXIT_AFTER_CALLBACK==1:
+				s.close()
+				break
+		print parts
+#end while
+
 
