844090031e0cff61c8270fe9b6687affc845eb34
raf <spamhole@telenet.be>
2013-04-10 15:19:45 +0200

    and again

---
+++ np_client_tftp.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+# TFTP conntrack: when succesfull it allows a callback from the server from another sourceport (same destination port)
+# it is not very usefull for exploitation, added as iptables nf_conntrack_tftp  check.
+#
+# Requires: scapy, must be run as root
+
+from scapy.all import *
+import socket
+
+class TFTPClient():
+	#wil send out one packet and wait for incomming packet from different source port to identify wether test was succesfull.
+	def __init__(self, server, serverport,sourceip,sourceport):
+		tftprq = IP(src=sourceip,dst=server)/UDP(sport=int(sourceport),dport=int(serverport))/TFTP()/TFTP_RRQ(filename="natpin.txt")
+		send(tftprq)
+	#end def
+#end class
+
+x = TFTPClient("62.213.198.42",69,"192.168.1.2",111) #portmapping for the win
+
