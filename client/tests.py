5146c8427680abb829035002c1e0e76a9e302867
allodoxaphobia <spamhole@telenet.be>
2013-04-10 16:32:18 +0300

    Update tests.py

--- client/tests.py
+++ client/tests.py
@@ -2,12 +2,12 @@
 from modules import irc
 from modules import ftp
 
-x = irc.Client("62.213.198.42", 6667,"192.168.1.2",9997)
+x = irc.Client("IP_HERE", 6667,"192.168.1.2",9997)
 x.run()
 if x.Result == 1: print "nf_conntrack_irc is loaded: natpin through DCC succesfull"
 else: print "nf_conntrack_irc is not loaded"
 
-x = ftp.Client("62.213.198.42", 21,"192.168.1.2",9998)
+x = ftp.Client("IP_HERE", 21,"192.168.1.2",9998)
 x.run()
 if x.Result == 1: print "nf_conntrack_ftp is loaded: natpin through PASV succesfull"
 else: print "nf_conntrack_ftp is not loaded"
