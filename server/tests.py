0def77ad00234fa8fbe91aa576c15d279bd99514
allodoxaphobia <spamhole@telenet.be>
2013-09-30 13:12:28 +0200

    adding server type all support

--- server/tests.py
+++ server/tests.py
@@ -11,18 +11,20 @@ def main():
     parser.add_option('-t', '--type', dest='cbtype', type=str, help='How do you wish to connect back to the client: socket, ssh, telnet')
     opts, args = parser.parse_args()
     try:
+	servers = []
     	if not opts.proto or not opts.cbtype:
     		print "Protocol and type are mandatory arguments"
     	else:
-	        if not opts.proto.upper() in ("FTP", "IRC"):
-	        	print "You need to specify a protocol, try FTP or IRC"
-	        else:
-	        	if opts.proto.upper() == "FTP":
-	        		x = ftp.Server(sCallbackType=opts.cbtype.lower())
-	        	elif opts.proto.upper() == "IRC":
-	        		x = irc.Server(sCallbackType=opts.cbtype.lower())
+		if opts.proto.upper() == "FTP":
+	        	servers.append(ftp.Server(sCallbackType=opts.cbtype.lower()))
+	        elif opts.proto.upper() == "IRC":
+	        	servers.append(irc.Server(sCallbackType=opts.cbtype.lower()))
+		else:#run all
+	        	servers.append(ftp.Server(sCallbackType=opts.cbtype.lower()))
+	        	servers.append(irc.Server(sCallbackType=opts.cbtype.lower()))
 		while True:
-	        	x.run()
+			for server in servers:
+				server.run()
     except Exception, e:
         print e
         sys.exit(1)
